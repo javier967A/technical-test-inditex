2f1387ba8cc05ffab9ee04d36854e718c4e2e4c7
Javier <javier.avile96@gmail.com>
2025-11-01 23:50:15 +0100

    creacion de las primeras clases en la capa de application

---
+++ src/main/java/com/inditex/technicaltest/features/prices/aplication/port/in/PriceUseCase.java
@@ -0,0 +1,4 @@
+package com.inditex.technicaltest.features.prices.aplication.port.in;
+
+public class PriceUseCase {
+}
---
+++ src/main/java/com/inditex/technicaltest/features/prices/aplication/port/out/PriceRepository.java
@@ -0,0 +1,4 @@
+package com.inditex.technicaltest.features.prices.aplication.port.out;
+
+public class PriceRepository {
+}
