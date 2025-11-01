be0507aabcf26ae057e4c52bdb3cefdec1c736a9
Javier <javier.avile96@gmail.com>
2025-11-01 23:08:56 +0100

    Primer coomit de inicio del proyecto

---
+++ src/test/java/com/inditex/technicaltest/TechnicaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.technicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicaltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
