72e73381460f288d7a24358f0754d64db29dd103
Javier <javier.avile96@gmail.com>
2025-11-02 20:24:48 +0100

    Cambio de Liquidbase a h2 con data sql

--- src/main/java/com/inditex/technicaltest/features/prices/infrastructure/web/controller/PriceController.java
+++ src/main/java/com/inditex/technicaltest/features/prices/infrastructure/web/controller/PriceController.java
@@ -1,13 +1,15 @@
 package com.inditex.technicaltest.features.prices.infrastructure.web.controller;
 
 import com.inditex.technicaltest.features.prices.aplication.port.in.PriceUseCase;
-import com.inditex.technicaltest.features.prices.infrastructure.web.dto.PriceRequest;
 import com.inditex.technicaltest.features.prices.infrastructure.web.dto.PriceResponse;
 import com.inditex.technicaltest.features.prices.infrastructure.web.mapper.PriceApiMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
